fe73ac983dc678c535ea10c7de1038a264537bf6
varadpathak2003 <varadpathak007@gmail.com>
2025-05-10 02:15:45 +0530

    Update AbtractFactoryPattern(Vehicles).java

--- AbtractFactoryPattern(Vehicles).java
+++ AbtractFactoryPattern(Vehicles).java
@@ -1,135 +1,91 @@
-interface engine {
+// Interfaces
+interface Engine {
     void start();
 }
-class TruckEngine implements engine{
-    @Override
-     public void start(){
-        System.out.println("Truck Engine Started");
-    }
+interface Brakes {
+    void apply();
 }
-class CarEngine implements engine{
-    @Override
-    public void start(){
-        System.out.println("Car Engine Started");
-    }
-}
-class MotorCyclesEngine implements engine{
-    @Override
-    public void start(){
-        System.out.println("Motorcycle Engine Started");
-    }
-}
-interface breaks{
-    public void apply();
+interface Transmissions {
+    void transmit();
 }
 
-class TruckBreak implements breaks{
-    @Override
-     public void apply(){
-        System.out.println("Truck Break Applied");
+// North America Car Components
+class NACarEngine implements Engine {
+    public void start() {
+        System.out.println("North America Car Engine Started");
     }
 }
-class CarBreak implements breaks{
-    @Override
-    public void apply(){
-        System.out.println("Car Break Applied");
+class NACarBrakes implements Brakes {
+    public void apply() {
+        System.out.println("North America Car Brakes Applied");
     }
 }
-class MotorcycleBreak implements breaks{
-    @Override
-    public void apply(){
-        System.out.println("Motorcycle Break Applied");
+class NACarTransmissions implements Transmissions {
+    public void transmit() {
+        System.out.println("North America Car Transmission");
     }
 }
 
-interface transmissions{
-    public void transmit();
-}
-
-class TruckTransmissions implements transmissions{
-    @Override
-    public void transmit(){
-         System.out.println("Truck Transmission");
+// Europe Truck Components
+class EuropeTruckEngine implements Engine {
+    public void start() {
+        System.out.println("Europe Truck Diesel Engine Started");
     }
 }
-class CarTransmissions implements transmissions{
-   @Override 
-    public void transmit(){
-         System.out.println("Car Transmission");
+class EuropeTruckBrakes implements Brakes {
+    public void apply() {
+        System.out.println("Europe Truck Brakes Applied");
     }
 }
-class MotorCyclesTransmissions implements transmissions{
-    @Override
-    public void transmit(){
-         System.out.println("MotorCycles Transmission");
+class EuropeTruckTransmissions implements Transmissions {
+    public void transmit() {
+        System.out.println("Europe Truck Transmission");
     }
 }
 
-interface VehicleFactory{
-    public void createEngine();
-    public void createBreaks();
-    public void createTransmissions();
+// Abstract Factory
+interface VehicleFactory {
+    Engine createEngine();
+    Brakes createBrakes();
+    Transmissions createTransmissions();
 }
 
- class TruckVehicleFactory  implements VehicleFactory{
-     @Override
-     public void createEngine(){
-         System.out.println(" Creating Truck Engine");
-     }
-     @Override
-     public void createBreaks(){
-         System.out.println(" Creating Truck Breaks");
-     }
-     @Override
-     public void createTransmissions(){
-         System.out.println(" Creating Truck Transmissions");
-     }
- }
- class MotorcycleVehicleFactory  implements VehicleFactory{
-     @Override
-     public void createEngine(){
-         System.out.println(" Creating Motorcycle Engine");
-     }
-     @Override
-     public void createBreaks(){
-         System.out.println(" Creating Motorcycle Breaks");
-     }
-     @Override
-     public void createTransmissions(){
-         System.out.println(" Creating Motorcycle Transmissions");
-     }
- }
- class CarVehicleFactory  implements VehicleFactory{
-     @Override
-     public void createEngine(){
-         System.out.println(" Creating Car Engine");
-     }
-     @Override
-     public void createBreaks(){
-         System.out.println(" Creating Car Breaks");
-     }
-     @Override
-     public void createTransmissions(){
-         System.out.println(" Creating Car Transmissions");
-     }
- }
+// Concrete Factories
+class NACarFactory implements VehicleFactory {
+    public Engine createEngine() {
+        return new NACarEngine();
+    }
+    public Brakes createBrakes() {
+        return new NACarBrakes();
+    }
+    public Transmissions createTransmissions() {
+        return new NACarTransmissions();
+    }
+}
 
+class EuropeTruckFactory implements VehicleFactory {
+    public Engine createEngine() {
+        return new EuropeTruckEngine();
+    }
+    public Brakes createBrakes() {
+        return new EuropeTruckBrakes();
+    }
+    public Transmissions createTransmissions() {
+        return new EuropeTruckTransmissions();
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        
-        VehicleFactory truckFactory=new TruckVehicleFactory();
-        truckFactory.createBreaks();
-        truckFactory.createEngine();
-        truckFactory.createTransmissions();
-        
-        VehicleFactory carFactory=new CarVehicleFactory();
-        carFactory.createBreaks();
-        carFactory.createEngine();
-        carFactory.createTransmissions();
-        
-        VehicleFactory motorcycleFactory=new MotorcycleVehicleFactory();
-        motorcycleFactory.createBreaks();
-        motorcycleFactory.createEngine();
-        motorcycleFactory.createTransmissions();
+        VehicleFactory naCarFactory = new NACarFactory();
+        Engine carEngine = naCarFactory.createEngine();
+        carEngine.start();
+        naCarFactory.createBrakes().apply();
+        naCarFactory.createTransmissions().transmit();
+
+        VehicleFactory euTruckFactory = new EuropeTruckFactory();
+        Engine truckEngine = euTruckFactory.createEngine();
+        truckEngine.start();
+        euTruckFactory.createBrakes().apply();
+        euTruckFactory.createTransmissions().transmit();
     }
 }
