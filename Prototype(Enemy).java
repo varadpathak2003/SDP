a4d211e7914fb4707a8bdef4c861844918c817d7
varadpathak2003 <varadpathak007@gmail.com>
2025-05-10 16:05:33 +0530

    Update Prototype(Enemy).java

--- Prototype(Enemy).java
+++ Prototype(Enemy).java
@@ -1,68 +1,43 @@
 //prototype interface
-interface Enemy{
+interface EnemyPrototype{
     Enemy clone();
     void spawn();
 }
 
 // concrete prototype
-class Zombie implements Enemy{
+class Enemy implements EnemyPrototype{
     private String type;
     private int health;
     private int attackPower;
     private String behaviour;
     
-    Zombie(String type,int health,int attackPower,String behaviour){
+    Enemy(String type,int health,int attackPower,String behaviour){
         this.type=type;
         this.health=health;
         this.attackPower=attackPower;
         this.behaviour=behaviour;
     }
     
-    @Override
-    public Zombie clone() {
-        return new Zombie(this.type, this.health, this.attackPower, this.behaviour);
-    }
-    
-    @Override
-    public void spawn(){
-        System.out.println("Zombie Spawned");
-    }
-}
-
-//concrete protoype
-class Alien implements Enemy{
-    private String type;
-    private int health;
-    private int attackPower;
-    private String behaviour;
-    
-    Alien(String type,int health,int attackPower,String behaviour){
+    public void setType(String type){
         this.type=type;
-        this.health=health;
-        this.attackPower=attackPower;
-        this.behaviour=behaviour;
     }
     
     @Override
-    public Alien clone(){
-        return new Alien(this.type, this.health, this.attackPower, this.behaviour);
+    public Enemy clone() {
+        return new Enemy(this.type, this.health, this.attackPower, this.behaviour);
     }
     
     @Override
     public void spawn(){
-        System.out.println("Alien Spawned");
+        System.out.println(this.type + " Spawned");
     }
 }
-//Client Code
 public class Main {
     public static void main(String[] args) {
-        Enemy zombiePrototype = new Zombie("Zombie", 100, 20, "Aggressive");
-        Enemy alienPrototype = new Alien("Alien", 300, 50, "Defensive");
+        Enemy enemy = new Enemy("Zombie", 100, 20, "Aggressive");
 
-        Enemy zombie1 = zombiePrototype.clone();
-        Enemy alien1 = alienPrototype.clone();
-
-        zombie1.spawn();
-        alien1.spawn();
+        Enemy alien = enemy.clone();
+        alien.setType("Alien");
+        alien.spawn();
     }
 }
